c8c1d8d80467d346de0efaea1991c58dee4e7002
Nobody082 <phamkhuyenno1@gmail.com>
2024-11-17 02:42:35 +0700

    Update bai71.c

--- bai71.c
+++ bai71.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#define endl printf("\n")
 int A[50000],B[50000];
 int size_A=0,size_B=0;
 void Merge()
@@ -27,6 +28,8 @@ int main()
     for(int j=0; j<size_B; j++)
     scanf("%d",&B[j]);
     for(int i=0; i<size_A; i++) printf("%d ", A[i]);
-    for(int j=0; j<size_B; j++) printf("%d ", B[j]); 
+    endl;
+    for(int j=0; j<size_B; j++) printf("%d ", B[j]); \
+    endl;
     Merge();
 }
