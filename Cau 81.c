c14921d2a03e442997516bdfeae66bb040a33a89
anhhoa242006 <nguyenanhhoa242006@gmail.com>
2024-11-30 11:28:31 +0700

    Update Cau 81.c

--- Cau 81.c
+++ Cau 81.c
@@ -1,28 +1,56 @@
 #include<stdio.h>
-#include<conio.h>
-#include<math.h>
-
-void nhap(long long n, long long matran[n][n]) {
-	for(long long i=0; i<n; i++) {
-		for(long long j=0; j<n; j++) {
-			scanf("%lld", &matran[i][j]);
-		}
-	}
+#define MAX 100
+void nhapMaTran(int a[MAX][MAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
 }
 
-void in(long long n, long long matran[n][n]) {
-	for(long long i=0; i<n; i++) {
-		for(long long j=0; j<n; j++) {
-			printf("%lld", matran[i][j]);
-		}
-	}
+void xuatMaTran(int a[MAX][MAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int Min(int a[MAX][MAX], int n) {
+    int tongMin = 0;
+    for (int i = 0; i < n; i++) {
+        int min = a[i][0];
+        int minIndex = 0;
+
+        for (int j = 1; j < n; j++) {
+            if (a[i][j] < min) {
+                min = a[i][j];
+                minIndex = j;
+            }
+        }
+
+        tongMin += min;
+
+        int temp = a[i][n - i - 1];
+        a[i][n - i - 1] = a[i][minIndex];
+        a[i][minIndex] = temp;
+    }
+    return tongMin;
 }
 
 int main() {
-	long long n;
-	scanf("%lld", &n);
-	long long A[n][n];
-	nhap(n, A);
-	in(n, A);
-	return 0;
+	int n;
+    int a[MAX][MAX];
+    do {
+        scanf("%d", &n);
+    } while (n <= 0);
+    nhapMaTran(a, n);
+    xuatMaTran(a, n);
+    int tongMin = Min(a, n);
+    printf("\n");
+    xuatMaTran(a, n);
+    printf("\n%d", tongMin);
+
+    return 0;
 }
