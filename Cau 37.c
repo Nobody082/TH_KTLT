b07c595a13b9c12964c3d53c2308efdb1bc618b8
anhhoa242006 <nguyenanhhoa242006@gmail.com>
2024-12-03 20:15:28 +0700

    Update Cau 37.c

--- Cau 37.c
+++ Cau 37.c
@@ -6,7 +6,7 @@ void chinhphuong(long long n) {
 	while(n > 0) {
 		long long k = (long long)sqrt(n);
 		long long s = k*k;
-		printf("%d^2", k);
+		printf("%lld^2", k);
 		n -= s;
 		if(n>0) printf(" + ");
 	}
