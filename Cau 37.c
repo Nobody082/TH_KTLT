e09c474441079ef0b9b8f2e563ef8cc5888c60a9
anhhoa242006 <nguyenanhhoa242006@gmail.com>
2024-12-03 20:45:32 +0700

    Update Cau 37.c

--- Cau 37.c
+++ Cau 37.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 void chinhphuong(long long n) {
-	while(n > 0) {
-		long long k = (long long)sqrt(n);
-		long long s = k*k;
-		printf("%lld^2", k);
-		n -= s;
-		if(n>0) printf(" + ");
-	}
-	printf("\n");
+    if (n <= 0) {
+        printf("Khong hop le.\n");
+        return;
+    }
+    while (n > 0) {
+        long long k = (long long)sqrt(n);  
+        long long s = k * k;               
+        printf("%lld^2", k);               
+        n -= s;                           
+        if (n > 0) printf(" + ");          
+    }
+    printf("\n");
 }
 
 int main() {
-	long long n;
-	scanf("%lld", &n);
-	chinhphuong(n);
-	return 0;
+    long long n;
+    scanf("%lld", &n);
+    chinhphuong(n);
+    return 0;
 }
