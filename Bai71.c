cace23f229ec615ec1a95251ffb9683d680279cc
Nobody082 <phamkhuyenno1@gmail.com>
2024-12-09 14:43:01 +0700

    Update Bai71.c

--- Bai71.c
+++ Bai71.c
@@ -2,34 +2,42 @@
 #define endl printf("\n")
 int A[50000],B[50000];
 int size_A=0,size_B=0;
-void Merge()
-{
-    int i=0, j=0;
-    while(i<size_A && j <size_B)
-        if(A[i]>=B[j])
-        {
-            printf("%d ", A[i]);
-            i++;
-        }
-        else
-        {
-            printf("%d ",B[j]);
-            j++;
-        }
-    for (;i<size_A; i++) printf("%d ",A[i]);
-    for (;j<size_B; j++) printf("%d ",B[j]);
-    return;
-}
-int main()
+void Init()
 {
     scanf("%d%d",&size_A, &size_B);
     for(int i=0; i<size_A; i++)
     scanf("%d",&A[i]);
     for(int j=0; j<size_B; j++)
     scanf("%d",&B[j]);
-    for(int i=0; i<size_A; i++) printf("%d ", A[i]);
+}
+void Print(int A[50000], size)
+{
+    for(int i=0; i<size; i++) printf("%d ", A[i]);
     endl;
-    for(int j=0; j<size_B; j++) printf("%d ", B[j]); \
-    endl;
-    Merge();
+}
+void Solve()
+{
+    int i=0, j=0;
+    int Res[500000], size=0;
+    while(i<size_A && j <size_B)
+        if(A[i]>=B[j])
+        {
+            Res[size++]=A[i];
+            i++;
+        }
+        else
+        {
+            Res[size++]=B[i];
+            j++;
+        }
+    Print(Res,size);
+    return;
+}
+
+int main()
+{
+    Init();
+    Print(A,size_A);
+    Print(B, size_B);
+    Solve();
 }
