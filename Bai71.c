b662c2fe2e8e567277db9306c2a74ad962e7d2d3
Nobody082 <phamkhuyenno1@gmail.com>
2024-12-12 20:14:38 +0700

    Update Bai71.c

--- Bai71.c
+++ Bai71.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
 #define endl printf("\n")
-int A[50000],B[50000];
-int size_A=0,size_B=0;
-void Init()
+void Init(int *A, int *B, int *size_A, int *size_B)
 {
-    scanf("%d%d",&size_A, &size_B);
-    for(int i=0; i<size_A; i++)
+    scanf("%d%d",size_A, size_B);
+    for(int i=0; i<*size_A; i++)
     scanf("%d",&A[i]);
-    for(int j=0; j<size_B; j++)
+    for(int j=0; j<*size_B; j++)
     scanf("%d",&B[j]);
 }
-void Print(int A[50000], int size)
+void Print(int *A, int *size)
 {
-    for(int i=0; i<size; i++) printf("%d ", A[i]);
+    for(int i=0; i<*size; i++) printf("%d ", A[i]);
     endl;
 }
-void Solve()
+void Solve(int A[], int B[], int size_A, int size_B)
 {
     int i=0, j=0;
     while(i<size_A && j <size_B)
@@ -38,8 +36,10 @@ void Solve()
 
 int main()
 {
-    Init();
-    Print(A,size_A);
-    Print(B,size_B);
-    Solve();
+    int A[50000],B[50000];
+    int size_A,size_B;
+    Init(A, B, &size_A, &size_B);
+    Print(A,&size_A);
+    Print(B,&size_B);
+    Solve(A, B, size_A, size_B);
 }
