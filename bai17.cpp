849278e6f2a24bdcacbb0f6d16b0fdbaccf519d5
khai-max <nguyentienkhai2910@gmail.com>
2024-12-13 13:55:26 +0700

    Update bai17.cpp

--- bai17.cpp
+++ bai17.cpp
@@ -1,31 +1,36 @@
-#include<stdio.h>
-#include<math.h>
-int min(int x,int y,int z) {
-	return  x<y&&x<z?x:(y<z?y:z);
+#include <stdio.h>
+#include <math.h>
+
+double min(double x, double y, double z) {
+    return x<y && x<z?x:(y<z?y:z);
+}
+double max(double x, double y, double z) {
+    return x>y && x>z?x:(y>z?y:z);
 }
 
-int max(int x,int y , int z){
-	return x>y&&x>z?x:(y>z?y:z); 
-} 
+int sosanh(double x, double y, double z) {
+    double sum = x+y+z;
+    double tich = x*y*z;
+    if (sum > tich) {
+        printf("b) min= %lf\n", tich);
+    } else {
+        printf("b) min= %lf\n", sum);
+    }
+    return 0;
+}
 
-int  sosanh(int x,int y,int z){
-   float sum= x+y+z;
-   float tich= x*y*z;
-   	if (sum>tich) {
-	   printf("b)min=%.0f\n",tich);}
-	else printf("b) min=%0.f\n",sum) ;
-	return 0; 
-}  
- int main()
- {
- 	float x,y,z,tich,S;
- 	scanf("%f%f%f",&x,&y,&z);
- 	  tich= x*y*z;
-	  S= (x+y+z)/2;
-	  printf("MIN %d\n",min(x,y,z));
-	  printf("MAX %d\n",max(x,y,z));
-	   sosanh(x,y,z); 
-	if (S>tich) printf("c) %.2f",(float)pow(tich,2)+1);
-	else printf("c)%.2f",(float)pow(S,2)+1) ;
- 	return 0;
- }
+int main() {
+    double x, y, z, tich, S;
+    scanf("%lf%lf%lf", &x, &y, &z);
+    tich = x*y*z;
+    S = (x+y+z)/2;
+    printf("MIN %lf\n", min(x, y, z));
+    printf("MAX %lf\n", max(x, y, z));
+    sosanh(x, y, z);
+    if (S > tich) {
+        printf("c) %.2lf\n", pow(tich, 2) + 1);
+    } else {
+        printf("c) %.2lf\n", pow(S, 2) + 1);
+    }
+    return 0;
+}
