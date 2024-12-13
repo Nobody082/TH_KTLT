5343b2949bc83e487958ea071ad84b5d0e0246c0
anhhoa242006 <nguyenanhhoa242006@gmail.com>
2024-12-13 20:20:26 +0700

    Update Cau 37.c

--- Cau 37.c
+++ Cau 37.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <limits.h>
 
-void chinhphuong(long long n) {
-    if (n <= 0) {
-        printf("Khong hop le.\n");
+void findMinSquares(int n) {
+    int *dp = (int *)malloc((n + 1) * sizeof(int));
+    int *squares = (int *)malloc((n + 1) * sizeof(int));
+
+    if (!dp || !squares) {
+        printf("Khong du bo nho!\n");
         return;
     }
-    while (n > 0) {
-        long long k = (long long)sqrt(n);  
-        long long s = k * k;               
-        printf("%lld^2", k);               
-        n -= s;                           
-        if (n > 0) printf(" + ");          
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = INT_MAX;
+        for (int j = 1; j * j <= i; j++) {
+            if (dp[i] > dp[i - j * j] + 1) {
+                dp[i] = dp[i - j * j] + 1;
+                squares[i] = j; 
+            }
+        }
+    }
+    int temp = n;
+    int count = 0;
+    int result[100]; 
+
+    while (temp > 0) {
+        result[count++] = squares[temp];
+        temp -= squares[temp] * squares[temp];
+    }
+    for (int i = count - 1; i >= 0; i--) {
+        printf("%d^2", result[i]);
+        if (i > 0) printf(" + ");
     }
     printf("\n");
+    free(dp);
+    free(squares);
 }
 
 int main() {
-    long long n;
-    scanf("%lld", &n);
-    chinhphuong(n);
+    int n;
+    scanf("%d", &n);
+    if (n < 0 || n > 7000000) {
+        printf("So khong hop le.\n");
+        return 1;
+    }
+    findMinSquares(n);
     return 0;
 }
